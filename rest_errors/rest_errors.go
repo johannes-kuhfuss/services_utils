20dafefe8c8d0d83275dfdbb09cc2fa0a4cd7295
johannes-kuhfuss <johannes.kuhfuss@web.de>
2021-10-14 10:05:24 +0200

    refactored to use IF, adjusted tests

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -1,41 +1,86 @@
 package rest_errors
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
 
-type RestErr struct {
-	Message    string        `json:"message"`
-	StatusCode int           `json:"statuscode"`
-	Causes     []interface{} `json:"causes"`
+type RestErr interface {
+	Message() string
+	StatusCode() int
+	Error() string
+	Causes() []interface{}
 }
 
-func NewError(msg string, code int) *RestErr {
-	return &RestErr{
-		Message:    msg,
-		StatusCode: code,
+type restErr struct {
+	ErrMessage    string        `json:"message"`
+	ErrStatusCode int           `json:"statuscode"`
+	ErrCauses     []interface{} `json:"causes"`
+}
+
+func (e restErr) Error() string {
+	return fmt.Sprintf("Message: %s - Status: %d - Causes: %v",
+		e.ErrMessage, e.ErrStatusCode, e.ErrCauses)
+}
+
+func (e restErr) Message() string {
+	return e.ErrMessage
+}
+
+func (e restErr) StatusCode() int {
+	return e.ErrStatusCode
+}
+
+func (e restErr) Causes() []interface{} {
+	return e.ErrCauses
+}
+
+func NewError(msg string, code int, causes []interface{}) RestErr {
+	return restErr{
+		ErrMessage:    msg,
+		ErrStatusCode: code,
+		ErrCauses:     causes,
 	}
 }
 
-func NewBadRequestError(msg string) *RestErr {
-	return &RestErr{
-		Message:    msg,
-		StatusCode: http.StatusBadRequest,
+func NewErrorFromBytes(bytes []byte) (RestErr, error) {
+	var restErr restErr
+	if err := json.Unmarshal(bytes, &restErr); err != nil {
+		return nil, errors.New("invalid json")
+	}
+	return restErr, nil
+}
+
+func NewBadRequestError(msg string) RestErr {
+	return restErr{
+		ErrMessage:    msg,
+		ErrStatusCode: http.StatusBadRequest,
 	}
 }
 
-func NewNotFoundError(msg string) *RestErr {
-	return &RestErr{
-		Message:    msg,
-		StatusCode: http.StatusNotFound,
+func NewNotFoundError(msg string) RestErr {
+	return restErr{
+		ErrMessage:    msg,
+		ErrStatusCode: http.StatusNotFound,
 	}
 }
 
-func NewInternalServerError(msg string, err error) *RestErr {
-	result := &RestErr{
-		Message:    msg,
-		StatusCode: http.StatusInternalServerError,
+func NewUnauthorizedError(msg string) RestErr {
+	return restErr{
+		ErrMessage:    msg,
+		ErrStatusCode: http.StatusUnauthorized,
+	}
+}
+
+func NewInternalServerError(msg string, err error) RestErr {
+	result := restErr{
+		ErrMessage:    msg,
+		ErrStatusCode: http.StatusInternalServerError,
 	}
 	if err != nil {
-		result.Causes = append(result.Causes, err.Error())
+		result.ErrCauses = append(result.ErrCauses, err.Error())
 	}
 	return result
 }
--- rest_errors/rest_errors_test.go
+++ rest_errors/rest_errors_test.go
@@ -9,43 +9,75 @@ import (
 )
 
 func TestNewError(t *testing.T) {
-	err := NewError("new error test", 55)
+	err := NewError("new error test", 55, nil)
 	assert.NotNil(t, err)
-	assert.EqualValues(t, "new error test", err.Message)
-	assert.EqualValues(t, 55, err.StatusCode)
-	assert.Nil(t, err.Causes)
+	assert.EqualValues(t, "new error test", err.Message())
+	assert.EqualValues(t, 55, err.StatusCode())
+	assert.Nil(t, err.Causes())
+}
+
+func TestError(t *testing.T) {
+	err := NewError("new error test", 54, nil)
+	errString := err.Error()
+	assert.EqualValues(t, errString, "Message: new error test - Status: 54 - Causes: []")
+}
+
+func TestNewErrorFromBytesError(t *testing.T) {
+	var bytes []byte
+	restErr, err := NewErrorFromBytes(bytes)
+	assert.NotNil(t, err)
+	assert.Nil(t, restErr)
+	assert.EqualValues(t, "invalid json", err.Error())
+}
+
+func TestNewErrorFromBytesNoError(t *testing.T) {
+	bytes := []byte("{\"message\":\"bytesTest\",\"statuscode\":56,\"causes\":null}")
+	restErr, err := NewErrorFromBytes(bytes)
+	assert.NotNil(t, restErr)
+	assert.Nil(t, err)
+	assert.EqualValues(t, "bytesTest", restErr.Message())
+	assert.EqualValues(t, 56, restErr.StatusCode())
+	assert.Nil(t, restErr.Causes())
 }
 
 func TestNewBadRequestError(t *testing.T) {
 	err := NewBadRequestError("new bad request error test")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, "new bad request error test", err.Message)
-	assert.EqualValues(t, http.StatusBadRequest, err.StatusCode)
-	assert.Nil(t, err.Causes)
+	assert.EqualValues(t, "new bad request error test", err.Message())
+	assert.EqualValues(t, http.StatusBadRequest, err.StatusCode())
+	assert.Nil(t, err.Causes())
 }
 
 func TestNewNotFoundError(t *testing.T) {
 	err := NewNotFoundError("new not found error test")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, "new not found error test", err.Message)
-	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
-	assert.Nil(t, err.Causes)
+	assert.EqualValues(t, "new not found error test", err.Message())
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode())
+	assert.Nil(t, err.Causes())
 }
 
 func TestNewInternalServerErrorNoExtraError(t *testing.T) {
 	err := NewInternalServerError("new internal server error test", nil)
 	assert.NotNil(t, err)
-	assert.EqualValues(t, "new internal server error test", err.Message)
-	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
-	assert.Nil(t, err.Causes)
+	assert.EqualValues(t, "new internal server error test", err.Message())
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode())
+	assert.Nil(t, err.Causes())
+}
+
+func TestNewUnauthorizedError(t *testing.T) {
+	err := NewUnauthorizedError("new unauthorized server error test")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "new unauthorized server error test", err.Message())
+	assert.EqualValues(t, http.StatusUnauthorized, err.StatusCode())
+	assert.Nil(t, err.Causes())
 }
 
 func TestNewInternalServerErrorWithExtraError(t *testing.T) {
 	err := NewInternalServerError("new internal server error test", errors.New("test error"))
 	assert.NotNil(t, err)
-	assert.EqualValues(t, "new internal server error test", err.Message)
-	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
-	assert.NotNil(t, err.Causes)
-	assert.EqualValues(t, 1, len(err.Causes))
-	assert.EqualValues(t, "test error", err.Causes[0])
+	assert.EqualValues(t, "new internal server error test", err.Message())
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode())
+	assert.NotNil(t, err.Causes())
+	assert.EqualValues(t, 1, len(err.Causes()))
+	assert.EqualValues(t, "test error", err.Causes()[0])
 }
