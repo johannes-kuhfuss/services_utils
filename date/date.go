beeb620b108beffeff6b719ef2023134b3c4237e
johannes-kuhfuss <johannes.kuhfuss@web.de>
2021-11-19 10:19:46 +0100

    added local time

--- date/date.go
+++ date/date.go
@@ -1,15 +1,44 @@
 package date
 
-import "time"
+import (
+	"strings"
+	"time"
+
+	"github.com/johannes-kuhfuss/services_utils/api_error"
+)
 
 const (
 	ApiDateLayout = time.RFC3339
 )
 
+func IsValidTime(timeStr string) bool {
+	_, err := time.Parse(ApiDateLayout, timeStr)
+	return err == nil
+}
+
 func GetNowUtc() time.Time {
 	return time.Now().UTC()
 }
 
+func GetNowLocal(location string) (*time.Time, api_error.ApiErr) {
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		return nil, api_error.NewBadRequestError("could not parse location")
+	}
+	localtime := time.Now().In(loc)
+	return &localtime, nil
+}
+
 func GetNowUtcString() string {
 	return GetNowUtc().Format(ApiDateLayout)
+}
+
+func GetNowLocalString(location string) (*string, api_error.ApiErr) {
+	localtime, err := GetNowLocal(strings.TrimSpace(location))
+	if err != nil {
+		return nil, err
+	} else {
+		localtimeStr := localtime.Format(ApiDateLayout)
+		return &localtimeStr, nil
+	}
 }
--- date/date_test.go
+++ date/date_test.go
@@ -11,10 +11,30 @@ func TestConst(t *testing.T) {
 	assert.EqualValues(t, time.RFC3339, ApiDateLayout)
 }
 
-func TestGetNowUtcString(t *testing.T) {
+func Test_GetNowUtcString_ReturnsNoErr(t *testing.T) {
 	now := GetNowUtcString()
 	date, err := time.Parse(ApiDateLayout, now)
 	assert.NotNil(t, date)
 	assert.Nil(t, err)
 	assert.EqualValues(t, date.Format(ApiDateLayout), now)
+}
+
+func Test_GetNowLocalString_WrongLocation_ReturnsErr(t *testing.T) {
+	nowLocal, err := GetNowLocalString("wrong location")
+	assert.Nil(t, nowLocal)
+	assert.NotNil(t, err)
+}
+
+func Test_GetNowLocalString_EmptyLocation_ReturnsNoErr(t *testing.T) {
+	nowLocal, err := GetNowLocalString("")
+	assert.NotNil(t, nowLocal)
+	assert.Nil(t, err)
+	assert.True(t, IsValidTime(*nowLocal))
+}
+
+func Test_GetNowLocalString_WithLocation_ReturnsNoErr(t *testing.T) {
+	nowLocal, err := GetNowLocalString("Europe/Berlin")
+	assert.NotNil(t, nowLocal)
+	assert.Nil(t, err)
+	assert.True(t, IsValidTime(*nowLocal))
 }
